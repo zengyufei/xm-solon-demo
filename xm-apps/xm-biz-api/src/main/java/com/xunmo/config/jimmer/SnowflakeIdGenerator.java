ab2b9d3612868f0e17b2de22f16e5a2f0c4776cd
zengyufei <312636208@qq.com>
2023-08-20 22:03:50 +0800

    提交中央仓

--- xm-apps/xm-biz-api/src/main/java/com/xunmo/config/jimmer/SnowflakeIdGenerator.java
+++ xm-apps/xm-biz-api/src/main/java/com/xunmo/config/jimmer/SnowflakeIdGenerator.java
@@ -1,13 +1,15 @@
 package com.xunmo.config.jimmer;
 
-import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.convert.Convert;
 import org.babyfish.jimmer.sql.meta.UserIdGenerator;
 
 public class SnowflakeIdGenerator implements UserIdGenerator<String> {
 
+	private IdWorker idWorker = new IdWorker(1L);
+
 	@Override
 	public String generate(Class<?> entityType) {
-		return IdUtil.getSnowflake().nextIdStr();
+		return Convert.toStr(idWorker.nextId());
 	}
 
 }
